dd1cb381e7502d0198e8bd887b63ef3979586f8a
OriginalDecode <originaldecode@outlook.com>
2019-05-22 17:39:52 +0200

    added more colors

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,45 @@
 import * as VS from 'vscode'
 import * as Parser from 'tree-sitter'
 
+const keywordsSpec = [
+	"if",
+	"do",
+	"class",
+	"struct",
+	"virtual",
+	"override",
+	"final",
+	"public",
+	"private",
+	"protected",
+	"nullptr",
+	"namespace",
+	"default",
+	"template",
+	"typename",
+	"const",
+	"return",
+	"sizeof",
+	"auto",
+	"for",
+]
+
 // Be sure to declare the language in package.json and include a minimalist grammar.
-const languages: {[id: string]: {parser: Parser, color: ColorFunction}} = {
+const languages: { [id: string]: { parser: Parser, color: ColorFunction } } = {
 	'go': createParser('tree-sitter-go', colorGo),
 	'typescript': createParser('tree-sitter-typescript', colorTypescript),
 	'cpp': createParser('tree-sitter-cpp', colorCpp),
 	'rust': createParser('tree-sitter-rust', colorRust),
 }
 
-type ColorFunction = (x: Parser.SyntaxNode, editor: VS.TextEditor) => {types: Parser.SyntaxNode[], fields: Parser.SyntaxNode[], functions: Parser.SyntaxNode[]}
+// type ColorMapping = { Map<string,Parser.SyntaxNode[]> }
+
+type ColorFunction = (x: Parser.SyntaxNode, editor: VS.TextEditor) => Map<string, Parser.SyntaxNode[]>
 
 function colorGo(x: Parser.SyntaxNode, editor: VS.TextEditor) {
+
+	var colorMapping = new Map<string, Parser.SyntaxNode[]>()
+
 	var types: Parser.SyntaxNode[] = []
 	var fields: Parser.SyntaxNode[] = []
 	var functions: Parser.SyntaxNode[] = []
@@ -30,10 +58,13 @@ function colorGo(x: Parser.SyntaxNode, editor: VS.TextEditor) {
 	}
 	scan(x)
 
-	return {types, fields, functions}
+	return colorMapping
 }
 
 function colorTypescript(x: Parser.SyntaxNode, editor: VS.TextEditor) {
+
+	var colorMapping = new Map<string, Parser.SyntaxNode[]>()
+
 	var types: Parser.SyntaxNode[] = []
 	var fields: Parser.SyntaxNode[] = []
 	var functions: Parser.SyntaxNode[] = []
@@ -52,10 +83,13 @@ function colorTypescript(x: Parser.SyntaxNode, editor: VS.TextEditor) {
 	}
 	scan(x)
 
-	return {types, fields, functions}
+	return colorMapping
 }
 
 function colorRust(x: Parser.SyntaxNode, editor: VS.TextEditor) {
+
+	var colorMapping = new Map<string, Parser.SyntaxNode[]>()
+
 	var types: Parser.SyntaxNode[] = []
 	var fields: Parser.SyntaxNode[] = []
 	var functions: Parser.SyntaxNode[] = []
@@ -78,23 +112,62 @@ function colorRust(x: Parser.SyntaxNode, editor: VS.TextEditor) {
 	}
 	scan(x)
 
-	return {types, fields, functions}
+	return colorMapping
 }
 
+function addToColorMap(map: Map<string, Parser.SyntaxNode[]>, id: string, node: Parser.SyntaxNode) {
+	let syntaxNodes = map.get(id);
+	if (!syntaxNodes) {
+		syntaxNodes = map.set(id, []).get(id)!;
+	}
+
+	syntaxNodes.push(node)
+}
+
+// @ts-ignore
 function colorCpp(x: Parser.SyntaxNode, editor: VS.TextEditor) {
-	var types: Parser.SyntaxNode[] = []
-	var fields: Parser.SyntaxNode[] = []
-	var functions: Parser.SyntaxNode[] = []
+
+	// var foo: { [accessParam: accessType]: containType } = { }
+
+	var colorMapping = new Map<string, Parser.SyntaxNode[]>()
+
 	function scan(x: Parser.SyntaxNode) {
 		if (!isVisible(x, editor)) return
-		if (x.type == 'identifier' && x.parent != null && x.parent.type == 'function_declarator') {
-			functions.push(x)
-		} else if (x.type == 'identifier' && x.parent != null && x.parent.type == 'scoped_identifier' && x.parent.parent != null && x.parent.parent.type == 'function_declarator') {
-			functions.push(x)
-		} else if (x.type == 'type_identifier') {
-			types.push(x)
-		} else if (x.type == 'field_identifier') {
-			fields.push(x)
+		if (x.type == 'identifier' ||
+			x.type == 'field_identifier') {
+			if (x.parent != null) {
+				if (x.parent.type == 'preproc_def') {
+					addToColorMap(colorMapping, "macros", x)
+				} else if (x.parent.type == 'destructor_name' ||
+					x.parent.type == 'function_declarator' ||
+					x.parent.type == 'call_expression') {
+					addToColorMap(colorMapping, 'functions', x)
+				} else if (x.parent.type == 'scoped_identifier' ||
+					x.parent.type == 'field_expression') {
+					if (x.parent.parent != null) {
+						if (x.parent.parent.type == 'function_declarator' ||
+							x.parent.parent.type == 'template_function' ||
+							x.parent.parent.type == 'call_expression') {
+							if (x.parent.parent.parent != null) {
+								addToColorMap(colorMapping, 'functions', x)
+							}
+						}
+					}
+				} else if (x.parent.type == 'enumerator') {
+					addToColorMap(colorMapping, 'enums', x)
+				} else if (x.parent.type == 'namespace_definition') {
+					addToColorMap(colorMapping, 'types', x)
+				}
+			} else if (x.type == 'field_identifier') {
+				addToColorMap(colorMapping, 'fields', x)
+			}
+		} else if (x.type == 'type_identifier' ||
+			x.type == 'namespace_identifier') {
+			addToColorMap(colorMapping, 'types', x)
+		} else if (x.type == 'primitive_type') {
+			addToColorMap(colorMapping, 'primitives', x)
+		} else if (keywordsSpec.includes(x.type)) {
+			addToColorMap(colorMapping, 'keywords', x)
 		}
 		for (const child of x.children) {
 			scan(child)
@@ -102,29 +175,29 @@ function colorCpp(x: Parser.SyntaxNode, editor: VS.TextEditor) {
 	}
 	scan(x)
 
-	return {types, fields, functions}
+	return colorMapping
 }
 
 function isVisible(x: Parser.SyntaxNode, editor: VS.TextEditor) {
 	for (const visible of editor.visibleRanges) {
-		const overlap = x.startPosition.row <= visible.end.line+1 && visible.start.line-1 <= x.endPosition.row
+		const overlap = x.startPosition.row <= visible.end.line + 1 && visible.start.line - 1 <= x.endPosition.row
 		if (overlap) return true
 	}
 	return false
 }
 
-function createParser(module: string, color: ColorFunction): {parser: Parser, color: ColorFunction} {
+function createParser(module: string, color: ColorFunction): { parser: Parser, color: ColorFunction } {
 	const lang = require(module)
 	const parser = new Parser()
 	parser.setLanguage(lang)
-	return {parser, color}
+	return { parser, color }
 }
 
 // Called when the extension is first activated by user opening a file with the appropriate language
 export function activate(context: VS.ExtensionContext) {
 	console.log("Activating tree-sitter...")
 	// Parse of all visible documents
-	const trees: {[uri: string]: Parser.Tree} = {}
+	const trees: { [uri: string]: Parser.Tree } = {}
 	function open(editor: VS.TextEditor) {
 		const language = languages[editor.document.languageId]
 		if (language == null) return
@@ -151,14 +224,14 @@ export function activate(context: VS.ExtensionContext) {
 			const startPosition = asPoint(startPos)
 			const oldEndPosition = asPoint(oldEndPos)
 			const newEndPosition = asPoint(newEndPos)
-			const delta = {startIndex, oldEndIndex, newEndIndex, startPosition, oldEndPosition, newEndPosition}
+			const delta = { startIndex, oldEndIndex, newEndIndex, startPosition, oldEndPosition, newEndPosition }
 			old.edit(delta)
 		}
 		const t = parser.parse(edit.document.getText(), old) // TODO don't use getText, use Parser.Input
 		trees[edit.document.uri.toString()] = t
 	}
 	function asPoint(pos: VS.Position): Parser.Point {
-		return {row: pos.line, column: pos.character}
+		return { row: pos.line, column: pos.character }
 	}
 	function close(doc: VS.TextDocument) {
 		if (doc.languageId == 'go') {
@@ -167,13 +240,25 @@ export function activate(context: VS.ExtensionContext) {
 	}
 	// Apply themeable colors
 	const typeStyle = VS.window.createTextEditorDecorationType({
-        color: new VS.ThemeColor('treeSitter.type')
+		color: new VS.ThemeColor('treeSitter.type')
 	})
 	const fieldStyle = VS.window.createTextEditorDecorationType({
-        color: new VS.ThemeColor('treeSitter.field')
+		color: new VS.ThemeColor('treeSitter.field')
 	})
 	const functionStyle = VS.window.createTextEditorDecorationType({
-        color: new VS.ThemeColor('treeSitter.function')
+		color: new VS.ThemeColor('treeSitter.function')
+	})
+	const primitiveStyle = VS.window.createTextEditorDecorationType({
+		color: new VS.ThemeColor('treeSitter.primitive')
+	})
+	const macroStyle = VS.window.createTextEditorDecorationType({
+		color: new VS.ThemeColor('treeSitter.macro')
+	})
+	const enumStyle = VS.window.createTextEditorDecorationType({
+		color: new VS.ThemeColor('treeSitter.enum')
+	})
+	const keywordStyle = VS.window.createTextEditorDecorationType({
+		color: new VS.ThemeColor('treeSitter.keyword')
 	})
 	function colorUri(uri: VS.Uri) {
 		for (const editor of VS.window.visibleTextEditors) {
@@ -187,10 +272,23 @@ export function activate(context: VS.ExtensionContext) {
 		if (t == null) return
 		const language = languages[editor.document.languageId]
 		if (language == null) return
-		const {types, fields, functions} = language.color(t.rootNode, editor)
-		editor.setDecorations(typeStyle, types.map(range))
-		editor.setDecorations(fieldStyle, fields.map(range))
-		editor.setDecorations(functionStyle, functions.map(range))
+		const colorMapping = language.color(t.rootNode, editor)
+
+		if (editor.document.languageId == 'cpp') {
+			if (colorMapping != null) {
+				editor.setDecorations(typeStyle, colorMapping.has('types') ? colorMapping.get('types')!.map(range) : [])
+				editor.setDecorations(fieldStyle, colorMapping.has('fields') ? colorMapping.get('fields')!.map(range) : [])
+				editor.setDecorations(functionStyle, colorMapping.has('functions') ? colorMapping.get('functions')!.map(range) : [])
+				editor.setDecorations(keywordStyle, colorMapping.has('keywords') ? colorMapping.get('keywords')!.map(range) : [])
+				editor.setDecorations(macroStyle, colorMapping.has('macros') ? colorMapping.get('macros')!.map(range) : [])
+				editor.setDecorations(enumStyle, colorMapping.has('enums') ? colorMapping.get('enums')!.map(range) : [])
+				editor.setDecorations(primitiveStyle, colorMapping.has('primitives') ? colorMapping.get('primitives')!.map(range) : [])
+			}
+		} else {
+			// if(types != null) editor.setDecorations(typeStyle, types.map(range))
+			// if(fields != null) editor.setDecorations(fieldStyle, fields.map(range))
+			// if(functions != null) editor.setDecorations(functionStyle, functions.map(range))
+		}
 		// console.log(t.rootNode.toString())
 	}
 	VS.window.visibleTextEditors.forEach(open)
@@ -205,4 +303,4 @@ function range(x: Parser.SyntaxNode): VS.Range {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
